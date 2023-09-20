3221759dfe15dd1747331ff8112050269b7a39a7
GHOST <ghostdevbusiness@gmail.com>
2023-09-20 23:42:52 +0000

    feat: basic search

---
+++ src/lib/Search.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	function submit(form: HTMLFormElement) {
+		const data = new FormData(form);
+		const station = data.get('station');
+
+		window.location.href = `/station/${station}`;
+	}
+</script>
+
+<form on:submit|preventDefault={(event) => submit(event.currentTarget)}>
+	<label>
+		<input
+			value="WAT"
+			type="text"
+			name="station"
+			placeholder="Station Code" />
+	</label>
+
+	<button>Search</button>
+</form>
