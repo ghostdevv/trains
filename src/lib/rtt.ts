a834b8687c450554aab950beacfbde631d2831f1
GHOST <ghostdevbusiness@gmail.com>
2024-07-29 00:02:45 +0100

    fix: maybe

--- src/lib/rtt.ts
+++ src/lib/rtt.ts
@@ -4,17 +4,13 @@ import { ofetch } from 'ofetch';
 const $fetch = ofetch.create({
 	baseURL: 'https://api.rtt.io/api/v1/json',
 	headers: {
-		Authorization: import.meta.env.RTT_AUTH,
+		Authorization: process.env.RTT_AUTH,
 	},
 });
 
 export async function rtt<T>(path: string) {
 	return $fetch<T>(path).catch((error) => {
-		console.error(
-			'rtt auth',
-			import.meta.env.SSR,
-			import.meta.env.RTT_AUTH,
-		);
+		console.error('rtt auth', import.meta.env.SSR, process.env.RTT_AUTH);
 		console.error('rtt fetch error', error);
 		return null;
 	});
