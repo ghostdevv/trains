ab643f08abab5329cc32a4136a77de053f887ca4
GHOST <ghostdevbusiness@gmail.com>
2024-07-28 23:41:22 +0100

    refactor: move fetchClasses call

--- src/lib/rtt.ts
+++ src/lib/rtt.ts
@@ -1,3 +1,4 @@
+import { db, eq, ClassCache } from 'astro:db';
 import { ofetch } from 'ofetch';
 
 const $fetch = ofetch.create({
@@ -13,4 +14,34 @@ export async function rtt<T>(path: string) {
 		console.error('rtt fetch error', error);
 		return null;
 	});
+}
+
+export async function fetchClasses(rttLink: string): Promise<number[]> {
+	const saved = await db
+		.select()
+		.from(ClassCache)
+		.where(eq(ClassCache.rtt_url, rttLink));
+
+	if (saved.length) {
+		return saved.map((record) => record.class_number);
+	}
+
+	const data = await ofetch(rttLink, { responseType: 'text' });
+	const matches = data.matchAll(/<span class="identity">(\d+)<\/span>/g);
+
+	const classes = Array.from(matches)
+		.flatMap(([, classNumber]) => classNumber && parseInt(classNumber))
+		.filter(
+			(classNumber): classNumber is number =>
+				typeof classNumber == 'number' && !isNaN(classNumber),
+		);
+
+	await db.insert(ClassCache).values(
+		classes.map((class_number) => ({
+			rtt_url: rttLink,
+			class_number,
+		})),
+	);
+
+	return classes;
 }
