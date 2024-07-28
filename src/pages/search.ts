14c5e0ec0c2af8ad9780a97fdc2ebdc747f86517
GHOST <ghostdevbusiness@gmail.com>
2024-07-28 22:15:16 +0100

    feat: improve search

--- src/pages/search.ts
+++ src/pages/search.ts
@@ -1,22 +1,22 @@
 import stations from '$lib/data/stations.json';
-import { ErrorMessage } from '$lib/errors';
 import { fzf } from '$lib/fuzzy-search';
 import type { APIRoute } from 'astro';
 
-function findCRS(query: string): string | null {
-	query = query.toLowerCase();
+function find(query: string): string | null {
+	query = query.trim().toLowerCase();
 
-	const result = stations.find(
-		(s) => [s.crs, s.tiploc].includes(query) || fzf(query, s.name),
-	);
+	const station =
+		stations.find((s) => s.crs == query) ||
+		stations.find((s) => s.tiploc == query) ||
+		stations.find((s) => fzf(query, s.name));
 
-	return result?.crs || null;
+	return station?.crs || null;
 }
 
-export const GET: APIRoute = ({ url, redirect }) => {
+export const GET: APIRoute = async ({ url, redirect }) => {
 	const query = url.searchParams.get('q');
 	if (!query) return redirect('/');
 
-	const crs = findCRS(query);
+	const crs = find(query);
 	return redirect(crs ? `/${crs}` : '/?error=search-missing-station');
 };
