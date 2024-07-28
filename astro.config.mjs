930f7df60c8367dd0438ee220108102bac1697c2
GHOST <ghostdevbusiness@gmail.com>
2024-07-28 19:58:59 +0100

    chore: use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [svelte()],
+	adapter: cloudflare(),
+	output: 'server',
 });
