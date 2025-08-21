2584501f58bb10581c507e70b6fbd081a04e4106
GHOST <ghostdevbusiness@gmail.com>
2025-08-21 23:12:30 +0100

    feat: station search now returns a results list if no exact match found

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,33 @@
-import { findStation } from '$lib/data/stations';
+import type { StationData } from '$lib/data/stations';
+import stations from '$lib/data/stations.json';
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
+import Fuse, { type FuseResult } from 'fuse.js';
+
+interface Result {
+	query: string;
+	matches: StationData | FuseResult<StationData>[];
+}
 
 export const server = {
 	stationSearch: defineAction({
 		accept: 'form',
 		input: z.object({
-			// tool: z.union([z.literal('live-trains'), z.literal('station-lookup')]),
 			query: z.string().min(1).max(128).trim().toLowerCase(),
 		}),
-		async handler({ query }) {
-			return findStation(query);
+		async handler({ query }): Promise<Result> {
+			const codeMatch = stations.find((s) => s.crs === query || s.tiploc === query);
+			if (codeMatch) return { query, matches: codeMatch as StationData };
+
+			const fuse = new Fuse<StationData>(stations, {
+				keys: [
+					{ name: 'name', weight: 3 },
+					{ name: 'crs', weight: 2 },
+					{ name: 'tiploc', weight: 1 },
+				],
+			});
+
+			return { query, matches: fuse.search(query, { limit: 8 }) };
 		},
 	}),
 };
--- src/lib/data/stations.ts
+++ src/lib/data/stations.ts
@@ -1,4 +1,3 @@
-import { fzf } from '$lib/fuzzy-search';
 import stations from './stations.json';
 
 export interface StationData {
@@ -7,19 +6,7 @@ export interface StationData {
 	tiploc?: string;
 }
 
-export function findStation(query: string): StationData | null {
-	// biome-ignore lint/style/noParameterAssign: it's ok
-	query = query.trim().toLowerCase();
-
-	const station =
-		stations.find((s) => s.crs == query) ||
-		stations.find((s) => s.tiploc == query) ||
-		stations.find((s) => fzf(query, s.name));
-
-	return station || null;
-}
-
-export function findByCRS(crs: string): StationData | null {
+export function getStation(crs: string): StationData | null {
 	if (crs.length !== 3) {
 		return null;
 	}
